51c51b9a3b9496dd59af3962a961d9af172bc4c1
Anjana.A <Anjana.A@ust.com>
2024-08-22 10:46:37 +0530

    Eleventh Commit

--- src/test/java/com/automation/pages/HomePage.java
+++ src/test/java/com/automation/pages/HomePage.java
@@ -1,7 +1,6 @@
 package com.automation.pages;
 
 import com.automation.utils.ConfigReader;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
--- src/test/java/com/automation/pages/ProductPage.java
+++ src/test/java/com/automation/pages/ProductPage.java
@@ -2,10 +2,8 @@ package com.automation.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
-import java.util.Set;
 
 public class ProductPage extends BasePage {
 
